5ffb09a80a92ea9d17ad00e2972b373cb88b3f83
zhuqing <zhuqinggooogle@gmail.com>
2013-05-23 17:10:15 +0800

    modified software&hardware

---
+++ inc/oa_debug.h
@@ -0,0 +1,36 @@
+/*****************************************************************************
+ *  Copyright Statement:
+ *  --------------------
+ *  MobileTech(Shanghai) Co., Ltd.
+ *
+ *****************************************************************************/
+
+/*****************************************************************************
+ *
+ * Filename:
+ * ---------
+ *   oa_debug.h
+ *
+ * Project:
+ * --------
+ *   OPEN AT project
+ *
+ * Description:
+ * ------------
+ *   This Module defines OPEN AT's basic types.
+ *
+ * Author:
+ * -------
+ *   zhuqing.
+ *
+ ****************************************************************************/
+#ifndef __OA_DEBUG_H__
+#define __OA_DEBUG_H__
+#define __DEBUG__ 
+
+#ifdef __DEBUG__  
+#define DEBUG(format,...) OA_DEBUG_USER(" "__FILE__" %s %d : "format"", __func__, __LINE__, ##__VA_ARGS__)
+#else  
+#define DEBUG(format,...)  
+#endif  
+#endif
