72f3b0a4c1eecc1548e6a9b475a73af4d759664a
zhuqing <zhuqinggooogle@gmail.com>
2013-06-21 16:48:57 +0800

    fix a bug in oa_area.c for 3 areas

--- src/Area_Judge.c
+++ src/Area_Judge.c
@@ -86,6 +86,7 @@ u8 SaveAreaData(u8 *pbuf,u8 area_type,u8 option,u16 *readlen)
 		//Reset_Area_ALL();//擦除所有信息	
 		//reset_all_area_file(area_type);
 		del_area_data(0/*has no effect*/, area_type, all_area);
+		option = 1;//删除后更新变为追加
 	}
 #if 0
 	if(area_manage.manage_vail_flag == 0xaa)//判断管理存储数据有效
