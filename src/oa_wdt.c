50c1a6c21ef5474d37ccd5665feeecdd807f3b90
zhuqing <zhuqinggooogle@gmail.com>
2013-10-29 14:10:30 +0800

    finish sim card time > 5min reboot

--- src/oa_wdt.c
+++ src/oa_wdt.c
@@ -39,12 +39,13 @@ extern DEV_PLAT_PARAS dev_running;
 void oa_app_online_offline(void)
 {
 	static u32 offline_times = 0;
+	static u32 sim_times = 0;
 	static oa_bool first = OA_FALSE;
 	if (OA_FALSE == first) {
 		DEBUG("(:(:(:(:(:(:(:(:task is %s running:):):):):):):):)", __func__);
 		first = OA_TRUE;
 	}
-	
+	//online/offline checkout
 	if (dev_running.plat_status == OFFLINE) {
 		offline_times++;
 		if (offline_times * OA_WDT_SCHEDULER_SECONDS > OFFLINE_RESTART_SHRESHOLD) {
@@ -53,6 +54,16 @@ void oa_app_online_offline(void)
 		}
 	} else if (dev_running.plat_status == ONLINE) {
 		offline_times = 0;
+	}
+	//sim card checkout
+	if (!oa_sim_network_is_valid()) {
+		sim_times++;
+		if (sim_times * OA_WDT_SCHEDULER_SECONDS > SIM_RESTART_SHRESHOLD) {
+			DEBUG("sim card is invalid time > 5min, reboot!");
+			oa_module_restart(NULL);
+		}
+	} else {
+		sim_times = 0;
 	}
 	oa_timer_start(OA_TIMER_ID_1, oa_app_online_offline, NULL, OA_WDT_SCHEDULER_PERIOD);
 	return;
